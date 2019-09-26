e2fe6ac6cabf7093e99c1fce1f4f7dee78efff82
徐路明 <xuluming@zailingtech.com>
2019-09-26 13:25:01 +0800

    update

--- src/com/pig/struct/Main.java
+++ src/com/pig/struct/Main.java
@@ -2,5 +2,7 @@ package com.pig.struct;
 public class Main{
     public static void main(String[] args) {
 
+        int year = 100,month = 100,day = 100,M = 100,B = 100;
+        int hashIndex = (( (year % M) * B + month ) % M * B + day ) % M;
     }
 }
--- src/com/pig/struct/trie/Trie.java
+++ src/com/pig/struct/trie/Trie.java
@@ -1,5 +1,92 @@
 package com.pig.struct.trie;
 
+import java.util.TreeMap;
+
+import javax.swing.plaf.TextUI;
+
 public class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    // 获得Trie中存储的单词数量
+    public int getSize() {
+        return size;
+    }
+
+
+    // 向Trie中添加一个新的单词word
+
+    /**
+     * @param word
+     */
+    public void add(String word) {
+        if (word == null || word.equals("")) return;
+        Node current = root;
+        int length = word.length();
+        for (int i = 0; i < length; i++) {
+            Character ch = word.charAt(i);
+            if (!current.next.containsKey(ch)) {
+                current.next.put(ch, new Node());
+            }
+            current = current.next.get(ch);
+        }
+
+        if (!current.isWord) {
+            current.isWord = true;
+            size++;
+        }
+    }
+
+    // 查询单词word是否在Trie中
+    public boolean contains(String word) {
+        if (word == null || word.equals("")) return false;
+        Node current = root;
+        int length = word.length();
+        for (int i = 0; i < length; i++) {
+            Character ch = word.charAt(i);
+            if (!current.next.containsKey(ch)) {
+                return false;
+            }
+            current = current.next.get(ch);
+        }
+        // 如果是没到叶子节点，判断中间节点是否是单词的结尾；
+        return current.isWord;
+    }
+
+
+    // 查询是否在Trie中有单词以prefix为前缀
+    public boolean isPrefix(String prefix){
+
+        Node cur = root;
+        for(int i = 0 ; i < prefix.length() ; i ++){
+            char c = prefix.charAt(i);
+            if(cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+
+        return true;
+    }
+
+
 
 }
