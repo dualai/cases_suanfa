f6c829f8a2ee5d01b3e226d2220a937fc8d07386
徐路明 <xuluming@zailingtech.com>
2019-09-27 09:27:05 +0800

    update

---
+++ src/com/pig/struct/hashtable/Main.java
@@ -0,0 +1,16 @@
+package com.pig.struct.hashtable;
+
+public class Main {
+
+    private final static int B = 26;
+    private final static int M = 12345;
+
+    public int strHashCode(String str){
+        int hashcode = 0;
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            hashcode = (hashcode * B + str.charAt(i));
+        }
+        return hashcode;
+    }
+}
